a365fbc941e0557dc202b9243f47b55ac6833054
Boris Mirkovic <boris.flashdev@gmail.com>
2025-08-05 17:56:36 +0200

    chore: initial commit

---
+++ include/utils/io/dataset.h
@@ -0,0 +1,24 @@
+﻿#ifndef DATASET_H
+#define DATASET_H
+
+typedef enum {
+    TYPE_DOUBLE,
+    TYPE_STRING
+} DataType;
+
+typedef struct {
+    char* name;
+    DataType type;
+    void** data;
+} Column;
+
+typedef struct {
+    Column* columns;
+    int n_cols;
+    int n_rows;
+} DataFrame;
+
+DataFrame* read_csv(const char* path, const char* separator);
+void free_dataframe(DataFrame* df);
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+#include "include/utils/io/dataset.h"
+
+int main() {
+    printf("test");
+    return 0;
+}
