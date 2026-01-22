32e420a3e0aa2c06067917a7d81d1490388bb49c
Boris <borismirkovic11@gmail.com>
2026-01-22 20:58:55 +0100

    chore: remove test code from main.c

--- main.c
+++ main.c
@@ -8,9 +8,5 @@ int main() {
     Matrix *m = read_csv("test.csv", ',', 1);
     matrix_print(m);
     matrix_shape(m);
-    printf("\n");
-    matrix_set(m, 3, 3, 67676767);
-    matrix_print(m);
-
     return 0;
 }
