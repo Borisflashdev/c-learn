9568a017d693a97bd4564116da18a5cc8a0bbc4f
Boris <borismirkovic11@gmail.com>
2026-02-17 22:36:15 +0100

    feat: add test file for feature scaling (standardization)

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
-﻿#include <math.h>
-
-#include "src/logistic_regression/logistic_regression.h"
-#include "src/matrix/matrix.h"
-
-int main() {
+﻿int main() {
     /**
     Tests are located in test_files/
     Note: .csv files must be placed in cmake-build-debug/ to be found at runtime
