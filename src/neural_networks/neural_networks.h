fb59d126f5851098599cb12588cb26acb3ceb6d5
Boris <borismirkovic11@gmail.com>
2026-02-17 23:35:21 +0100

    feat: add neural network module structure and header

---
+++ src/neural_networks/neural_networks.h
@@ -0,0 +1,51 @@
+﻿#ifndef NEURAL_NETWORKS_H
+#define NEURAL_NETWORKS_H
+#include "../matrix/matrix.h"
+#include "../vector/vector.h"
+
+typedef enum {
+    GradientDescent,
+    Adam
+} Optimizer;
+
+typedef enum {
+    BinaryCrossEntropy,
+    CategoricalCrossEntropy,
+    MSE
+} LossFunction;
+
+typedef enum {
+    ReLU,
+    LeakyReLU,
+    SiLU,
+    Sigmoid,
+    Tanh,
+    Softmax,
+} Activation;
+
+typedef struct DenseLayer {
+    char *name;
+    int units;
+    Matrix *coef;
+    Vector *intercepts;
+    Activation activation;
+} DenseLayer;
+
+typedef struct {
+    int random_seed;
+    int num_layers;
+    DenseLayer **layers;
+    LossFunction loss_function;
+    Optimizer optimizer;
+    double learning_rate;
+} NeuralNetwork;
+
+NeuralNetwork neural_network_create(int num_layers, LossFunction loss_function, Optimizer optimizer, int random_seed);
+void neural_network_free(NeuralNetwork *neural_network);
+void neural_network_describe(NeuralNetwork *neural_network);
+
+void neural_network_add_layer(NeuralNetwork *neural_network, int units, Activation activation, char *name);
+void neural_network_fit(NeuralNetwork *neural_network, Matrix *X, Matrix *y, int epochs, double learning_rate);
+Matrix *neural_network_predict(NeuralNetwork *neural_network, Matrix *X);
+
+#endif
